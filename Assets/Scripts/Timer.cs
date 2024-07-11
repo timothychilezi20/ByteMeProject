e4a487e61598595bf82bceb59ca3d413982b8715
padifalasanerd <163738111+padifalas@users.noreply.github.com>
2024-07-11 19:12:55 +0200

    Timer ???

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,31 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class TimerController : MonoBehaviour
+public class Timer : MonoBehaviour
 {
     private float timer = 30f; 
     public TMP_Text timerText; 
+    private bool timerActive = false; 
+   
 
     void Start()
     {
         UpdateTimerText();
+        Debug.Log("start is called");
+
     }
 
     void Update()
     {
-        if (timer > 0)
+        
+        if (timerActive)
         {
-            timer -= Time.deltaTime;
-            UpdateTimerText();
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+                UpdateTimerText();
+            }
+            else
+            {
+
+                Debug.Log("Time's up!");
+                timer = 30f; 
+                timerActive = false; 
+                UpdateTimerText(); 
+                
+               
+            }
         }
-        else
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject collidedObject = other.gameObject;
+
+
+        if (collidedObject.CompareTag("Player"))
         {
-            
-            Debug.Log("Time's up!");
-            timer = 30f; 
-            UpdateTimerText(); 
+            timerActive = true; 
+            Debug.Log("Player entered enemy zone, timer started.");
         }
     }
 
@@ -34,6 +55,6 @@ public class TimerController : MonoBehaviour
         int minutes = Mathf.FloorToInt(timer / 60);
         int seconds = Mathf.FloorToInt(timer % 60);
 
-        timerText.text = string.Format("Time remaining: {0:00}:{1:00}", minutes, seconds); 
+        timerText.text = string.Format("Time: {0:00}:{1:00}", minutes, seconds);
     }
 }
